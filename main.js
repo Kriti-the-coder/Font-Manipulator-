63a532f80be2fddc134d53f9fb946e0af1f87a09
Kriti-the-coder <89910822+Kriti-the-coder@users.noreply.github.com>
2022-08-16 15:41:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 difference = 0;
 rightWristX = 0;
 leftWristX = 0;
@@ -6,8 +5,10 @@ leftWristX = 0;
   function setup() {
   video = createCapture(VIDEO);
   video.size(550, 500);
-   canvas = createCanvas(550, 550);
+
+  canvas = createCanvas(550, 550);
   canvas.position(560,150);
+
   poseNet = ml5.poseNet(video, modelLoaded);
   poseNet.on('pose', gotPoses);
 }
@@ -26,14 +27,16 @@ function gotPoses(results)
     leftWristX = results[0].pose.leftWrist.x;
     rightWristX = results[0].pose.rightWrist.x;
     difference = floor(leftWristX - rightWristX);
+
     console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
   }
 }
 
 function draw() {
-  background('#969A97');
-  textsize(difference);
-  text('Kriti',50,400);
-  fill('#FFE787');
-  document.getElementById("font").innerHTML = "=" + difference +"px";
+background('#6C91C2');
+
+  document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference +"px";
+textSize(difference);
+fill('#FFE787');
+text('Kriti', 50, 400);
 }
